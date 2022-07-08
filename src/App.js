7c2619242392a79dc6a73b0e8ea92e131a7fe852
Berlin <Berline_lucien@yahoo.com>
2022-07-08 00:39:06 -0400

    app.js created, added search text and toggle mode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import NotesList from "./components/NotesList";
+import { nanoid } from "nanoid";
+import Search from "./components/Search";
+import Header from "./components/Header";
 
 function App() {
+  {
+    /** adding there dummy notes in our useState hooks for notes */
+  }
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "This is my first note",
+      date: "03/25/2022",
+    },
+    {
+      id: nanoid(),
+      text: "Adding another note",
+      date: "03/30/2022",
+    },
+    {
+      id: nanoid(),
+      text: "Buy more food",
+      date: "04/25/2022",
+    },
+  ]);
+
+  {
+    /** searching text and toggle mode */
+  }
+  const [searchNote, setSearchNote] = useState("");
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={""} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
